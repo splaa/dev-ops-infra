065b6bab8c5ec27e7689c29ac244481ce9dd34bc
Андрей Лавринюк <splaandrey@gmail.com>
2020-04-18 14:29:37 +0300

    + Added line Света Hello

--- run.sh
+++ run.sh
@@ -4,3 +4,7 @@
 echo "Hello, SplX-2020"
 echo "I am Success!!!"
 echo "I am Света Hello!!!"
+echo "I am Света Hello!!!"
+echo "I am Света Hello!!!"
+echo "I am Света Hello!!!"
+echo "I am Света Hello!!!"
